22184648e57cf9c402d487f400650adc0581cf31
John S <116411179+SolarKnight0@users.noreply.github.com>
2025-04-08 15:03:51 -0400

    tft board base creation

--- src/App.js
+++ src/App.js
@@ -3,8 +3,27 @@ import "./App.css";
 
 function App() {
   return (
-    <div>
-      <p>sup</p>
+    <div className="main">
+      <div className="container">
+        {Array.from({ length: 7 }).map((_, i) => (
+          <div key={`r1-${i}`} />
+        ))}
+      </div>
+      <div className="container offset">
+        {Array.from({ length: 7 }).map((_, i) => (
+          <div key={`r2-${i}`} />
+        ))}
+      </div>
+      <div className="container">
+        {Array.from({ length: 7 }).map((_, i) => (
+          <div key={`r3-${i}`} />
+        ))}
+      </div>
+      <div className="container offset">
+        {Array.from({ length: 7 }).map((_, i) => (
+          <div key={`r2-${i}`} />
+        ))}
+      </div>
     </div>
   );
 }
